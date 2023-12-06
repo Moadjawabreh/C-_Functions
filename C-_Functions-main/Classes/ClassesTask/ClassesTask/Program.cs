69b10f57ef76f932b8de49548fbb1565bc49b184
Moad Jawabreh <moadjawabreh2001@gmail.com>
2023-12-06 14:35:27 +0300

    Add files via upload

---
+++ C-_Functions-main/Classes/ClassesTask/ClassesTask/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTask
+{
+    class Task1
+    {
+        public Task1()
+        {
+            Console.WriteLine("MyClass class has initialized");
+        }    
+    }
+
+    class Task2
+    {
+        public Task2(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class Task3
+    {
+        public  long CalculateFactorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Factorial is not defined for negative numbers.");
+            }
+
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            long factorial = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+
+
+
+        public class ArraySorter
+        {
+        public  void PrintArray(int[] array)
+        {
+            Console.Write("{ ");
+
+            Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine(" }");
+        }
+        }
+
+        public class Difference
+        {
+        
+        public void DisplayTheDifference(DateTime dateTime1,DateTime dateTime2)
+        {
+            TimeSpan timeSpan=dateTime1 - dateTime2;
+            Console.WriteLine(timeSpan.ToString());
+        }
+
+        }
+
+    public class StringToDateConversion
+    {
+       
+        public  DateTime ConvertStringToDate(string dateString)
+        {
+            string format = "dd-MM-yyyy";
+
+            DateTime convertedDate = DateTime.ParseExact(dateString, format, null);
+
+            return convertedDate;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //  Task1 display = new Task1();
+
+            //2
+            //Task2 display2 = new Task2("Hello");
+
+            //3
+            //  Task3 display = new Task3();
+            //int number = 5;
+            //long result = display.CalculateFactorial(number);
+
+            //4
+            //int[] array = { 11, -2, 4, 35, 0, 8, -9 };
+            //ArraySorter sorter = new ArraySorter();
+            //Console.WriteLine("Sorted array:");
+            //sorter.PrintArray(array);
+
+            //5
+            //Difference difference = new Difference();
+            //DateTime date1 = new DateTime(1981, 11, 03);
+            //DateTime date2 = new DateTime(2013, 09, 04);
+
+            //difference.DisplayTheDifference(date1, date2);
+
+            //6
+            StringToDateConversion stringToDate=new StringToDateConversion();
+            string dateString = "12-08-2004";
+            DateTime convertedDate = stringToDate.ConvertStringToDate(dateString);
+            Console.WriteLine($"Converted Date: {convertedDate.ToString("yyyy-MM-dd")}");
+        }
+    }
+}
---
+++ C-_Functions-main/Functions/Functions2/Functions2/Program.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //int number = Add(10, 20);
+
+            //2
+            //float newNumber=removeLeadingTrailing("230.000");
+
+            //3
+            //int[] nums = new int[] { 10, 40, 30, 20, 50 };
+            //int secondLarge = secondLargest(nums);
+
+            //4
+            //Console.WriteLine(isRepdigit(66));   
+            //Console.WriteLine(isRepdigit(0));     
+            //Console.WriteLine(isRepdigit(-11));
+
+            //5
+
+            //string newString=reverseWords(" the sky is blue");
+
+            //6
+            //int[] array1 = { 1, 2, 3, 4, 5, 6, 7 };
+            //Console.WriteLine(SevenBoom(array1));
+
+            //7
+
+            //string newWord=insertWhitespace("SheWalksToTheBeach");
+
+            //8
+
+            //bool[] array1 = { true, false, false, true, false };
+            //Console.WriteLine(CountTrue(array1));
+
+            //9
+
+            //Console.WriteLine(CapToFront("hApPy"));      
+
+
+            //10
+
+            //string[] strings = { "rsq", "6hi", "g", "rsq6hig" };
+            //bool check= matchLastItem(strings);
+
+            //11
+            //Console.WriteLine(FindNaN(new double[] { 1, 2, double.NaN }));       
+
+            //12
+            // string[] stringsWithDuplicateItems = { "John", "Taylor", "John" };
+            //string[] stringsWithOutDuplicateItems = removeDups(stringsWithDuplicateItems);
+
+            // Console.WriteLine(stringsWithOutDuplicateItems);
+
+            //13
+
+            //string timeConverted = convertTime("12:40:22AM");
+
+            //14
+            //string stringAfter= removeLastVowel("Those who dare to fail miserably can achieve greatly.");
+
+
+            //15
+            int[] numbers = { 1, 5, 9 };
+
+            Console.WriteLine(SumOfCubes(numbers));
+        }
+
+
+        static int SumOfCubes(int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += (int)Math.Pow(number, 3);
+            }
+
+            return sum;
+        }
+
+        private static string removeLastVowel(string sentence)
+        {
+            List<Char> list = new List<Char>();
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == 'e' || sentence[i] == 'o' || sentence[i] == 'a' || sentence[i] == 'i' || sentence[i] == 'u')
+                {
+                    if (sentence[i+1]==' ')
+                    {
+                        continue;
+                    }
+
+                }
+                list.Add(sentence[i]);
+
+            }
+             sentence = string.Join("", list);
+
+            return sentence;    
+        }
+
+        private static string convertTime(string time)
+        {
+            string hours = time[0].ToString() + time[1].ToString();
+            int intHours=int.Parse(hours);
+
+
+            foreach (var item in time)
+            {
+                if (item == 'P')
+                {
+                    intHours=intHours+12;
+                    if (intHours == 24)
+                    {
+                        intHours = 12;
+                    }
+
+                    time = intHours + time.Substring(2, 6);
+                }
+                else if(item == 'A')
+                {
+                    if (intHours == 12)
+                    {
+                        intHours = intHours - 12;
+
+                    }
+                    time = intHours + time.Substring(2, 6);
+
+                }
+
+            }
+            return time;
+        }
+
+        private static string[] removeDups(string [] strings)
+        {
+            HashSet<string> result = new HashSet<string>();
+            foreach (var item in strings)
+            {
+                result.Add(item);   
+            }
+            return result.ToArray();
+        }
+
+        static int FindNaN(double[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (double.IsNaN(numbers[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+
+        private static bool matchLastItem(string[] strings)
+        {
+            string v= strings[strings.Length-1];
+            string concat = "";
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (i == strings.Length - 1)
+                {
+                    if (concat == strings[i].ToString())
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                concat += strings[i];
+                
+            }
+            return false;
+
+        }
+
+        static string CapToFront(string word)
+        {
+            StringBuilder capitalLetters = new StringBuilder();
+            StringBuilder nonCapitalLetters = new StringBuilder();
+
+            foreach (char letter in word)
+            {
+                if (char.IsUpper(letter))
+                {
+                    capitalLetters.Append(letter);
+                }
+                else
+                {
+                    nonCapitalLetters.Append(letter);
+                }
+            }
+
+            return capitalLetters.ToString() + nonCapitalLetters.ToString();
+        }
+
+
+        static int CountTrue(bool[] values)
+        {
+            int count = 0;
+
+            foreach (bool value in values)
+            {
+                if (value)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static string insertWhitespace(string v)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (char.IsUpper(v[i]) && i > 0)
+                {
+                    sb.Append(' ');
+                }
+                sb.Append(v[i]);
+
+            }
+            return sb.ToString();
+        }
+
+        static string SevenBoom(int[] numbers)
+        {
+            foreach (int num in numbers)
+            {
+                if (ContainsSeven(num))
+                {
+                    return "Boom!";
+                }
+            }
+
+            return "there is no 7 in the array";
+        }
+
+        static bool ContainsSeven(int number)
+        {
+            while (number > 0)
+            {
+                if (number % 10 == 7)
+                {
+                    return true;
+                }
+                number /= 10;
+            }
+
+            return false;
+        }
+
+
+
+        private static string reverseWords(string v)
+        {
+            string[] words = v.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+
+        }
+
+        static bool isRepdigit(int number)
+        {
+            string numberString = Math.Abs(number).ToString();
+
+            for (int i = 1; i < numberString.Length; i++)
+            {
+                if (numberString[i] != numberString[0])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static int secondLargest(int [] values)
+        {
+            Array.Sort(values);
+            Array.Reverse(values);
+            return values[1];
+        }
+
+        private static float removeLeadingTrailing(string v)
+        {
+            float num = float.Parse(v);
+            return num;
+        }
+
+        private static int Add(int v1, int v2)
+        {
+            return v1+v2;
+        }
+    }
+}
